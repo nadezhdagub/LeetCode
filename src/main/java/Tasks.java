b30199be24bb640c7bc368492c79d9831f6a88ed
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2022-08-31 00:30:25 +0300

    Add files via upload

--- src/main/java/Tasks.java
+++ src/main/java/Tasks.java
@@ -31,6 +31,53 @@ class TwoSum {
     }
 }
 
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class AddTwoNumbers {
+    /**
+     * 2) You are given two non-empty linked lists representing two non-negative integers.
+     * The digits are stored in reverse order, and each of their nodes contains a single digit.
+     * Add the two numbers and return the sum as a linked list.
+     * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+     */
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+        return dummy.next;
+    }
+}
+
 class ThreeHundredAndTwentySix {
     /**
      * 326) Given an integer n, return true if it is a power of three. Otherwise, return false.
@@ -98,9 +145,12 @@ class ReorderedPowerOfTwo {
 public class Tasks {
     public static void main(String[] args) {
         ThreeHundredAndFortyTwo.isPowerOfFour(17);
+
         ThreeHundredAndTwentySix.isPowerOfThree(9);
+
         int[] xu = {2, 6, 8, 7, 5};
         TwoSum.twoSum(xu, 10);
+
         System.out.println(ReorderedPowerOfTwo.reorderedPowerOf2(1));
         System.out.println(ReorderedPowerOfTwo.reorderedPowerOf2(10));
         System.out.println(ReorderedPowerOfTwo.reorderedPowerOf2(16));
@@ -109,5 +159,6 @@ public class Tasks {
         System.out.println(ReorderedPowerOfTwo.reorderedPowerOf2(368407186));
         System.out.println(ReorderedPowerOfTwo.reorderedPowerOf2(679213508));
         System.out.println(ReorderedPowerOfTwo.reorderedPowerOf2(850239671));
+
     }
 }
