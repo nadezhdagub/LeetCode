896a988037bb40898c72a09d0060f0912c68d2aa
Nadezhda Gubenko <41242817+nadezhdagub@users.noreply.github.com>
2022-10-06 00:46:15 +0300

    Add files via upload

--- src/main/java/Tasks.java
+++ src/main/java/Tasks.java
@@ -400,7 +400,7 @@ class ContainerWithMostWater {
 class IntegerToRoman {
     /**
      * 12) Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
-     *
+     * <p>
      * Symbol
      * I 1
      * V 5
@@ -411,21 +411,32 @@ class IntegerToRoman {
      * M 1000
      * For example, 2 is written as II in Roman numeral, just two one's added together.
      * 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
-     *
+     * <p>
      * Roman numerals are usually written largest to smallest from left to right.
      * However, the numeral for four is not IIII. Instead, the number four is written as IV.
      * Because the one is before the five we subtract it making four. The same principle applies
      * to the number nine, which is written as IX. There are six instances where subtraction is used:
-     *
+     * <p>
      * I can be placed before V (5) and X (10) to make 4 and 9.
      * X can be placed before L (50) and C (100) to make 40 and 90.
      * C can be placed before D (500) and M (1000) to make 400 and 900.
      * Given an integer, convert it to a roman numeral.
+     *
      * @param num
      * @return
      */
-    public String intToRoman(int num) {
+    private static String[] ROMENUMBER = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    private static int[] VALUES = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
 
+    public static String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < ROMENUMBER.length; i++) {
+            while (num >= VALUES[i]) {
+                sb.append(ROMENUMBER[i]);
+                num -= VALUES[i];
+            }
+        }
+        return sb.toString();
     }
 }
 
@@ -823,5 +834,7 @@ public class Tasks {
         System.out.println(RegularExpressionMatching.isMatch("aa", "a*"));
 
         System.out.println(WildcardMatching.isMatch("aaaa", "***a") + " T");
+
+        System.out.println(IntegerToRoman.intToRoman(6));
     }
 }
